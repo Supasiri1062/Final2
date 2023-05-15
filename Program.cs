81708910276b5608c33b54c321d5f4542e2d7736
unknown <supasiri.benj@mail.kmutt.ac.th>
2023-05-15 16:36:28 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class program{
+    static void Main(string[] args) {
+        int Vmax = int.Parse(Console.ReadLine());
+        int Vfill = int.Parse(Console.ReadLine());
+        int Vdrink = int.Parse(Console.ReadLine());
+        int tday = int.Parse(Console.ReadLine());
+        int tdrink = int.Parse(Console.ReadLine());
+        int tfill = int.Parse(Console.ReadLine());
+
+        Console.Write("Vmax: ");
+        Console.Write("Vdrink: ");
+        Console.Write("Vfill: ");
+
+        Console.Write("tday: ");
+        Console.Write("tfill: ");
+        Console.Write("tdrink: ");
+
+        if(Vfill == tdrink){
+            
+        }
+    }
+}
